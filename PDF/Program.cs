049e034306a0d2e96227c744a440c0c8437ff4af
Antto <coutreelantoine@gmail.com>
2023-05-26 12:00:26 +0200

    Ajout de test dans le main

--- PDF/Program.cs
+++ PDF/Program.cs
@@ -1,7 +1,34 @@
 ﻿using PDF;
 
-// Instanciation d'un poule Ginger
-Poule ginger = new Poule("Ginger", "gauloise", 0.8, Taille.L);
+// Ajout des variables pour le poulailler
+string nomPoulailler;
+int capacitéMax;
 
-// Affichage de cette poule
-ginger.Afficher();
+// Ajout des variables pour la poule
+string nomPoule;
+string race;
+int intensite;
+
+// Instanciation d'un poulailler
+Console.WriteLine("Nom du poulailler : ");
+nomPoulailler = Console.ReadLine();
+Console.WriteLine("Capacité maximal : ");
+capacitéMax = Convert.ToInt32(Console.ReadLine());
+
+Poulailler poulailler1 = new Poulailler(nomPoulailler, capacitéMax);
+
+// Instanciation des poules
+Console.WriteLine("Nom de la poule : ");
+nomPoule = Console.ReadLine();
+Console.WriteLine("La race : ");
+race = Console.ReadLine();
+Console.WriteLine("Intensite de la ponte : ");
+intensite = Convert.ToInt32(Console.ReadLine());
+
+Poule pouleP = new Poule(nomPoule, race, intensite, Taille.L);
+
+//Poulailler.Ajout(pouleP);
+
+
+// Affichage de ce poulailler
+poulailler1.Affiche();
